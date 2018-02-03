d71f1f1838199c02a680ecffb9f75c1a544a509d
captmajac <36032330+captmajac@users.noreply.github.com>
2018-02-03 20:01:19 +0100

    Update module.php

--- FTS12/module.php
+++ FTS12/module.php
@@ -22,7 +22,7 @@
 			//Connect to available enocean gateway
 			$this->ConnectParent("{A52FEFE9-7858-4B8E-A96E-26E15CB944F7}");
 			
-			$this->SetReceiveDataFilter(".*"\"DeviceID\":". hexdec($this->ReadPropertyString("DeviceID")).".*");
+			$this->SetReceiveDataFilter(".*\"DeviceID\":".hexdec($this->ReadPropertyString("DeviceID")).".*");
 		}
 		
 		/*
