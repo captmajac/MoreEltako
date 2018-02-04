0359c6a262161bf73e2f85370b136f401c9bab7c
captmajac <36032330+captmajac@users.noreply.github.com>
2018-02-04 14:08:18 +0100

    Update module.php

--- FSB14/module.php
+++ FSB14/module.php
@@ -94,40 +94,47 @@
 			$letztezeit=GetValue($this->GetIDForIdent("Fahrzeit"));
 			if ($letztezeit > $this->ReadPropertyString("Pos100"))
 			{	// höher als das ende. dann komplett unten
+				IPS_LogMessage("FSB14 Pos",">100");
 				SetValue($this->GetIDForIdent("Positon"), 100);
 			}
 			else if ($letztezeit > $this->ReadPropertyString("Pos99"))
 			{	// höher als das 99. dann zwischen 99 und 100
+				IPS_LogMessage("FSB14 Pos",">99");
 				$step = $this->ReadPropertyString("Pos100") - $this->ReadPropertyString("Pos99");
 				$step2 = ($letztezeit - $this->ReadPropertyString("Pos100")) * (24/$step) + 0;
 				SetValue($this->GetIDForIdent("Positon"), $step2);
 			}
 			else if ($letztezeit > $this->ReadPropertyString("Pos75"))
 			{	// höher als das 75. dann zwischen 75 und 99
+				IPS_LogMessage("FSB14 Pos",">75");
 				$step = $this->ReadPropertyString("Pos99") - $this->ReadPropertyString("Pos75");
 				$step2 = ($letztezeit - $this->ReadPropertyString("Pos99")) * (25/$step) + 0;
 				SetValue($this->GetIDForIdent("Positon"), $step2);
 			}
 			else if ($letztezeit > $this->ReadPropertyString("Pos50"))
 			{	// höher als das 50. dann zwischen 50 und 75
+				IPS_LogMessage("FSB14 Pos",">50");
 				$step = $this->ReadPropertyString("Pos75") - $this->ReadPropertyString("Pos50");
 				$step2 = ($letztezeit - $this->ReadPropertyString("Pos75")) * (25/$step) + 0;
 				SetValue($this->GetIDForIdent("Positon"), $step2);
 			}
 			else if ($letztezeit > $this->ReadPropertyString("Pos25"))
 			{	// höher als das 25. dann zwischen 25 und 50
+				IPS_LogMessage("FSB14 Pos",">25");
 				$step = $this->ReadPropertyString("Pos50") - $this->ReadPropertyString("Pos25");
 				$step2 = ($letztezeit - $this->ReadPropertyString("Pos50")) * (25/$step) + 0;
 				SetValue($this->GetIDForIdent("Positon"), $step2);
 			}
 			else if ($letztezeit > $this->ReadPropertyString("Pos0"))
 			{	// höher als das 0. dann zwischen 0 und 25
+				IPS_LogMessage("FSB14 Pos",">0");
 				$step = $this->ReadPropertyString("Pos25") - $this->ReadPropertyString("Pos0");
 				$step2 = ($letztezeit - $this->ReadPropertyString("Pos25")) * (25/$step) + 0;
 				SetValue($this->GetIDForIdent("Positon"), $step2);
 			}
 			else if ($letztezeit < $this->ReadPropertyString("Pos0"))
 			{	// kleiner als das 0. dann ganz oben
+				IPS_LogMessage("FSB14 Pos","<0");
 				SetValue($this->GetIDForIdent("Positon"), 0);
 			}
 		}
