c34b11e33e46ca3a05749b451deb93d6e911262e
MArC <MArC@192.168.0.11>
2021-01-07 18:59:19 +0100

    .

--- Generic/module.php
+++ Generic/module.php
@@ -131,7 +131,7 @@ class GenericEEP extends IPSModule {
 								"Reference": "todo"
 							    }]
 								},
-								{ "type": "Button", "caption": "Apply selcted", "onClick": "print_r($devices['ID']);"
+								{ "type": "Button", "caption": "Apply selcted", "onClick": "print_r($devices['name']);"
 								}
 						    ]
 						}
