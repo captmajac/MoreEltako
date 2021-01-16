59763514ad1c83e4e84c32b74ec28e7e373019f4
MArC <jacobi@me.com>
2021-01-16 11:34:02 +0100

    .

--- Generic/module.php
+++ Generic/module.php
@@ -152,9 +152,9 @@ class GenericEEP extends IPSModule {
 
 		// Add Info alle Daten anzeigen
 		$AddInfo = strtoupper(str_pad(dechex ( $data->{'DataByte3'}), 2, 0, STR_PAD_LEFT) );
-		$AddInfo = $AddInfo.strtoupper(str_pad(dechex ( $data->{'DataByte2'}), 2, 0, STR_PAD_LEFT) );
-		$AddInfo = $AddInfo.strtoupper(str_pad(dechex ( $data->{'DataByte1'}), 2, 0, STR_PAD_LEFT) );
-		$AddInfo = $AddInfo.strtoupper(str_pad(dechex ( $data->{'DataByte0'}), 2, 0, STR_PAD_LEFT) );
+		$AddInfo = $AddInfo."-".strtoupper(str_pad(dechex ( $data->{'DataByte2'}), 2, 0, STR_PAD_LEFT) );
+		$AddInfo = $AddInfo."-".strtoupper(str_pad(dechex ( $data->{'DataByte1'}), 2, 0, STR_PAD_LEFT) );
+		$AddInfo = $AddInfo."-".strtoupper(str_pad(dechex ( $data->{'DataByte0'}), 2, 0, STR_PAD_LEFT) );
 		$newValue->Reference = $AddInfo;
 		
 		if (@in_array($newValue->Ident , array_column($values, 'Ident') ) == false)
